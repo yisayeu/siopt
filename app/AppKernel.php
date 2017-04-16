ddc444ed1bee817edf7546357891e572d83bd41c
Yury Isayeu <yisayeu@gmail.com>
2017-04-16 16:39:05 +0200

    Generated BlogFrontendBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
             new BlogBundle\BlogBundle(),
+            new Blog\FrontendBundle\BlogFrontendBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ src/Blog/FrontendBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Blog\FrontendBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('BlogFrontendBundle:Default:index.html.twig');
+    }
+}
